79156394b9a1a83ab004d4b5d7cef53b133b6f42
Mark Laukkanen <mark.laukkanen@gmail.com>
2016-09-16 20:42:56 +0300

    Create topbar content and layout. Add interactivity to buttons.

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    
+    $(".toggleButton").hover(function() {
+        $(this).toggleClass("hoverOver");                    
+    })
+   
+    $(".toggleButton").click(function() {
+        $(this).toggleClass("active");
+    })
+    
+    
+    
+    
+    
+})
